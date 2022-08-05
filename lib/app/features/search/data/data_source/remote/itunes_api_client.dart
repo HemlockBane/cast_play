e4273afd47779a82a6f3d91cc15bfa0c817fd03a
Obinna Igwe <igwenus619@gmail.com>
2022-08-05 13:01:44 +0100

    Working on itunes api client test

--- lib/app/features/search/data/data_source/remote/itunes_api_client.dart
+++ lib/app/features/search/data/data_source/remote/itunes_api_client.dart
@@ -1,5 +1,5 @@
-
 import 'package:dio/dio.dart' hide Headers;
+import 'package:dio/native_imp.dart';
 import 'package:injectable/injectable.dart';
 import 'package:podplay_flutter/app/features/search/data/model/response/podcast_response.dart';
 import 'package:podplay_flutter/core/data/data_source/remote/api_client.dart';
@@ -11,16 +11,13 @@ part 'itunes_api_client.g.dart';
 @Injectable()
 @RestApi(baseUrl: ApiConfig.iTunesUrl)
 abstract class ItunesApiClient {
-
   @factoryMethod
   factory ItunesApiClient.fromApiClient(ApiClient apiClient) =>
       ItunesApiClient(apiClient.dio);
 
   factory ItunesApiClient(Dio dio, {String baseUrl}) = _ItunesApiClient;
 
-  @Headers(<String, dynamic>{
-    "content-type": "text/javascript; charset=utf-8"
-  })
+  @Headers(<String, dynamic>{"content-type": "text/javascript; charset=utf-8"})
   @GET("/search?media=podcast")
   Future<PodcastResponse> getPodcastByTerm(@Query("term") String term);
 }
